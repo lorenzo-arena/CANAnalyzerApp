d2b14b126b6526045d26454c826d387ab39c0d5c
Lorenzo Arena <lorenzo.arena@dimsport.it>
2019-04-05 18:38:21 +0200

    Aggiunte pagine di base con tema iniziale

--- CANAnalyzerApp/CANAnalyzerApp/Models/HomeMenuItem.cs
+++ CANAnalyzerApp/CANAnalyzerApp/Models/HomeMenuItem.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace CANAnalyzerApp.Models
 {
     public enum MenuItemType
     {
-        Browse,
-        About
+        CANSpyOne,
+        CANSpyTwo,
+        KLineSpy
     }
     public class HomeMenuItem
     {
         public MenuItemType Id { get; set; }
 
         public string Title { get; set; }
+        public Color Foreground { get; set; }
     }
 }
